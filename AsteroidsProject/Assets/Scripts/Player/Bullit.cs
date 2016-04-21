2b2d285be0adbe0dc31592dbcb3c18206574078b
DBeumans <buncho.gamingnetwork@gmail.com>
2016-04-21 12:10:22 +0200

    Added PlayerMovement
    Added playermovement, playershooting script, player look at mouse
    script.

---
+++ AsteroidsProject/Assets/Scripts/Player/Bullit.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullit : MonoBehaviour {
+
+    [SerializeField]
+    float speed;
+	// Use this for initialization
+	void Start ()
+    {
+        //Destroy(gameObject, 0.40f);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+
+    public void SetSpeed(float value)
+    {
+        speed = value;
+    }
+}
--- AsteroidsProject/Assets/Scripts/Player/PlayerMovement.cs
+++ AsteroidsProject/Assets/Scripts/Player/PlayerMovement.cs
@@ -3,13 +3,58 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    // Use this for initialization
+
+    [SerializeField]
+    float playerspeed = 10f;
+    [SerializeField]
+    float jumpPower = 20f;
+
+    [SerializeField]
+    bool grounded = false;
+
+    [SerializeField]
+    Transform  groundedEnd;
+
+    Rigidbody2D rb2d;
+
+    void Start ()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        PlayerMovementHandler();
+        raycasting();
 	}
+
+    void raycasting()
+    {
+        Debug.DrawLine(this.transform.position, groundedEnd.position, Color.green);
+
+        grounded = Physics2D.Linecast(this.transform.position, groundedEnd.position, 1 << LayerMask.NameToLayer("Ground"));
+
+    }
+
+    void PlayerMovementHandler()
+    {
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * 4f * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.right * 4f * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && grounded == true)
+        {
+            rb2d.AddForce(Vector2.up * jumpPower);
+        }
+    }
 }
