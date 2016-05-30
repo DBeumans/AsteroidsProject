05dbe22779d8a1060ffc6409169339d33a04ed52
DBeumans <buncho.gamingnetwork@gmail.com>
2016-05-30 02:02:37 +0200

    Commit Danilo

--- AsteroidsProject/Assets/Scripts/Enemies/EnemyAi.cs
+++ AsteroidsProject/Assets/Scripts/Enemies/EnemyAi.cs
@@ -9,25 +9,31 @@ public class EnemyAi : MonoBehaviour {
     Transform GroundedCheck;
 
     [SerializeField]
-    bool PlayerInSight;
+    Transform FieldOfViewFront;
     [SerializeField]
+    Transform FieldOfViewBack;
+
+    [SerializeField]
+    bool PlayerInSight;
+
+    bool PlayerIsBehind;
+    [SerializeField]
+
     bool grounded;
 
     bool Left;
     bool Right = true;
-    bool PlayerIsBehind;
+
+    bool FieldOfViewFrontBool;
+    bool FieldOfViewBackBool;
 
     bool isFacingLeft;
     bool isFacingRight;
 
-    // Use this for initialization
-    void Start()
+    void Update()
     {
-        
+        CheckPlayer();
     }
-
-    // Update is called once per frame
-
     void FixedUpdate()
     {
         raycasting();
@@ -36,8 +42,12 @@ public class EnemyAi : MonoBehaviour {
     void raycasting()
     {
         Debug.DrawLine(this.transform.position, GroundedCheck.position, Color.green); // Check ground
+        Debug.DrawLine(this.transform.position, FieldOfViewFront.position, Color.red); 
+        Debug.DrawLine(this.transform.position, FieldOfViewBack.position, Color.blue); 
 
         grounded = Physics2D.Linecast(this.transform.position, GroundedCheck.position, 1 << LayerMask.NameToLayer("Ground")); // Check ground
+        FieldOfViewFrontBool = Physics2D.Linecast(this.transform.position, FieldOfViewFront.position, 1 << LayerMask.NameToLayer("Player"));
+        FieldOfViewBackBool = Physics2D.Linecast(this.transform.position, FieldOfViewBack.position, 1 << LayerMask.NameToLayer("Player"));
     }
 
     void EnemyTransform()
@@ -68,6 +78,57 @@ public class EnemyAi : MonoBehaviour {
         
     }
 
+    void CheckPlayer()
+    {
+        if(FieldOfViewFrontBool)
+        {
+            ChasePlayer(true);
+            Debug.Log("Chasing The Player!");
+        }
+        if (FieldOfViewBackBool)
+        {
+            ChasePlayer(false);
+            Debug.Log("Enemy Flipped!");
+        }
+        
+        if (FieldOfViewFrontBool == false && FieldOfViewBackBool == false)
+        {
+            Debug.Log("Nothing! Both Are False!");
+        }
+
+    }
+
+    void FlipEnemy()
+    {
+        if(isFacingLeft)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+        if(isFacingRight)
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+    }
+
+    void ChasePlayer(bool value)
+    {
+        if(value) // Rechts
+        {
+            Right = false;
+            Left = false;
+            transform.Translate(Vector2.right * MovementSpeed * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+        if(!value) // Links
+        {
+            Right = false;
+            Left = false;
+            transform.Translate(Vector2.right * MovementSpeed * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+        
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.CompareTag("GoLeft"))
@@ -86,5 +147,6 @@ public class EnemyAi : MonoBehaviour {
         {
             Physics2D.IgnoreLayerCollision(10, 10);
         }
+
     }
 }
