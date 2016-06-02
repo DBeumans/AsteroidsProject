5139783d04c8dfe8b0b1914e8c2c6a9faaf01fed
DBeumans <buncho.gamingnetwork@gmail.com>
2016-06-02 13:58:27 +0200

    Commit danilo

---
+++ AsteroidsProject/Assets/Scripts/World/Spawning_back.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawning_back : MonoBehaviour {
+
+    [SerializeField]
+    Transform[] spawnPoints;
+    [SerializeField]
+    GameObject enemy;
+    [SerializeField]
+    float SpawnTime = 0.01f;
+
+    bool EnableSpawn = false;
+
+    PlayerMovement player;
+
+    // Use this for initialization
+    void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+        if (EnableSpawn)
+        {
+            InvokeRepeating("Spawn", SpawnTime, SpawnTime);
+        }
+    }
+    void Update()
+    {
+        if (player.gameObject.layer == LayerMask.NameToLayer("Player1"))
+        {
+            EnableSpawn = true;
+        }
+        if (player.gameObject.layer != LayerMask.NameToLayer("Player1"))
+        {
+            EnableSpawn = false;
+        }
+    }
+    void Spawn()
+    {
+
+        // Find a random index between zero and one less than the number of spawn points.
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+}
---
+++ AsteroidsProject/Assets/Scripts/World/Spawning_front.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawning_front : MonoBehaviour {
+
+    [SerializeField]
+    Transform[] spawnPoints;
+    [SerializeField]
+    GameObject enemy;
+    [SerializeField]
+    float SpawnTime = 0.01f;
+
+    bool EnableSpawn = false;
+
+    PlayerMovement player;
+    // Use this for initialization
+    void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+        if(EnableSpawn)
+        {
+            InvokeRepeating("Spawn", SpawnTime, SpawnTime);
+        }
+    }
+    void Update()
+    {
+        if (player.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            EnableSpawn = true;
+        }
+        if(player.gameObject.layer != LayerMask.NameToLayer("Player"))
+        {
+            EnableSpawn = false;
+        }
+    }
+    void Spawn()
+    {
+
+        // Find a random index between zero and one less than the number of spawn points.
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+}
